084713aaddb68d40346c2cfd242d429e2d3d61ad
Cristian <95197107+cristianaros@users.noreply.github.com>
2023-08-25 12:36:24 -0400

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,17 @@
+def main():
+    num = int(input("Ingrese un número: "))
+    if es_primo(num):
+        print(f"{num} es primo")
+    else:
+        print(f"{num} no es primo")
+
+def es_primo(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    main()
